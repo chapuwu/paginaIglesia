0823693f9fffb891b4a3cc8e29ab50f36f788305
chapuwu <lolochapa10@hotmail.com>
2024-12-14 10:29:09 -0300

    se agrego una pantalla de aviso de la pagina que esta en desarollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import logoBody from '../public/img/logo2.png'
 import BigButton from './components/BigButton'
 import Conocenos from './components/Conocenos'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
+    const [aviso, setAviso] = useState(false)
     const conocenosRef = useRef()
 
-    return (
-        <div id='app' className='bg-orange-100 h-screen w-screen overflow-auto'>
+    return aviso ? (
+        <div id='app' className='h-screen w-screen bg-orange-100 overflow-auto'>
             <Header />
             <main className='flex justify-center w-full'>
                 <div className='container flex items-center flex-col'>
@@ -24,6 +25,26 @@ function App() {
                 </div>
             </main>
             <Footer></Footer>
+        </div>
+    ) : (
+        <div className='bg-orange-100 h-screen w-screen flex justify-center items-center'>
+            <div className='bg-orange-200 h-1/2 w-1/2 flex flex-col items-center rounded-lg justify-evenly'>
+                <h1 className='text-logo text-2xl font-bold font-sans'>Atencion, Esta pagina esta en Desarrollo!</h1>
+                <div className='bg-orange-100 w-1/2 flex justify-center h-1/2 items-center'>
+                    <p className='text-xl bg-orange-100 w-11/12'>
+                        <b>Este proyecto esta en desarollo</b> y con el tiempo se va a ir agregando mas cosas y
+                        arreglando errores visuales para dispositivos moviles y con errores visuales, esto es solo una
+                        version incompleta del proyecto, Saludos y Bendiciones
+                    </p>
+                </div>
+                <div className='w-28 h-14 flex justify-center'>
+                    <button
+                        className='text-white font-sans text-xl h-full w-full bg-logo hover:bg-red-600 transition-all'
+                        onClick={() => setAviso(true)}>
+                        Aceptar
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
